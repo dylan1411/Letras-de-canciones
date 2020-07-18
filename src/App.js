5602346552adaa7ca042528f61dbccd8a30806ce
Dylan <dachaconp@unal.edu.co>
2020-07-18 11:35:51 -0500

    Mostrand artista

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cancion from './components/Cancion';
+import Artista from './components/Artista';
+import axios from 'axios';
 
 function App() {
   //state
   const [search, setSearch] = useState({});
   const [letra, guardarLetra] = useState('');
+  const [info, guardarInfo] = useState('');
   //useEffect
   useEffect(() => {
     if(Object.keys(search).length === 0) return;
     const {artista, cancion} = search;
     const consultarAPIletra = async () => {
       const url=`https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-      const respuesta = await fetch(url);
-      const resultado = await respuesta.json();
+      const url2=`https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+      
+      const [letra, informacion] = await Promise.all([
+        axios(url),
+        axios(url2)
+      ])
 
-      guardarLetra(resultado.lyrics)
+      guardarLetra(letra.lyrics);
+      guardarInfo(informacion.data.artists[0]);
     }
     consultarAPIletra()
-  }, [search])
+  }, [search, info])
   return (
     <Fragment>
       <Formulario
@@ -28,7 +36,7 @@ function App() {
       <div className="container mt-5">
         <div className="row">
           <div className="col-md-6">
-            1
+            <Artista info={info} />
           </div>
           <div className="col-md-6">
             { letra ? <Cancion letra={letra} /> : null }
